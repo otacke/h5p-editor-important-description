15e744c34cbed12bdffcfb39483aa752a3e88839
Oliver Tacke <o.tacke@posteo.de>
2024-04-11 13:01:17 +0200

    Modernize build chain

--- src/entries/dist.js
+++ src/entries/dist.js
@@ -1,5 +1,5 @@
 import '@styles/h5peditor-important-description.scss';
-import ImportantDescription from '@scripts/h5peditor-important-description';
+import ImportantDescription from '@scripts/h5peditor-important-description.js';
 
 // Load library
 H5PEditor.widgets.importantDescription = ImportantDescription;
